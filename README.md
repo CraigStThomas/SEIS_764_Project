3e8d898c9473fb356b4bfcc2b54d84bc8c7dcc4c
Eric Helander <hela6565@stthomas.edu>
2019-12-08 18:49:14 -0600

    Rename first 10 images in A01, fix reactPhotoViewer bug, add reactphotoViewer README notes

--- README.md
+++ README.md
@@ -2,10 +2,28 @@
 
 a repository for the group project in SEIS 764, Fall 2019 semester
 
-## matlabMotionBasedMultipleObjectTracking
+## [cell_cycle_detector](cell_cycle_detector)
+
+- TODO: Write instructions.
+
+## [LSTM_frame_prediction](LSTM_frame_prediction)
+
+- TODO: Write instructions.
+
+## [matlabMotionBasedMultipleObjectTracking](matlabMotionBasedMultipleObjectTracking)
+
+- TODO: Write instructions.
 
 ## reactPhotoViewer
 
-- Install node, npm, yarn.
-- Run `yarn` in the directory to install the dependencies.
-- Run `yarn start`
+- The browser-based React Photo Viewer application enables a user to view images sequentially, with full control over the number of milliseconds per frame. This is particularly useful for observing changes between frames, such as the phase change observed in image family `A06` between images `764` and `765`.
+  - ![react-photo-viewer_A06_764](README-img/react-photo-viewer_A06_764.png)
+  - ![react-photo-viewer_A06_765](README-img/react-photo-viewer_A06_765.png)
+- Prerequisites for running the application:
+  - Install [Node.js](https://nodejs.org/en/)
+  - Install [yarn](https://yarnpkg.com/lang/en/docs/install/)
+- To run the application:
+  1. `cd` into the `reactPhotoViewer` directory.
+  2. Run `yarn` to install the application's dependencies.
+  3. Run `yarn start` to start the application.
+  4. Navigate to [http://localhost:3000/](http://localhost:3000/)
--- reactPhotoViewer/src/Photos.js
+++ reactPhotoViewer/src/Photos.js
@@ -1,31 +1,35 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from "react";
 
 export default function Photos() {
-  const [imageNumber, setImageNumber] = useState('0');
+  const [imageNumber, setImageNumber] = useState("00");
   const [intervalValue, setIntervalValue] = useState(500);
-  const [imageFamily, setImageFamily] = useState('A01');
-  // const [imageSrc, setImageSrc] = useState(require(`./img/${imageNumber}_${imageFamily}_blended.png`));
-  const [imageSrc, setImageSrc] = useState(require(`../../images/${imageFamily}/${imageNumber}_${imageFamily}_blended.png`));
+  const IMAGE_FAMILIES = ["A01", "A02", "A03", "A04", "A05", "A06"];
+  const [imageFamily, setImageFamily] = useState(IMAGE_FAMILIES[0]);
+  const [imageSrc, setImageSrc] = useState(
+    require(`../../images/${imageFamily}/${imageNumber}_${imageFamily}_blended.png`)
+  );
   const [isPaused, setIsPaused] = useState(false);
 
   useEffect(() => {
-    setImageSrc(require(`../../images/${imageFamily}/${imageNumber}_${imageFamily}_blended.png`));
-  }, [imageFamily, imageNumber])
+    setImageSrc(
+      require(`../../images/${imageFamily}/${imageNumber}_${imageFamily}_blended.png`)
+    );
+  }, [imageFamily, imageNumber]);
 
   const handleSetImageNumber = useCallback(imageInt => {
     function handleSetImageNumber(imageInt) {
      if (imageInt >= 1 && imageInt <= 788) {
-       const newImageString = imageInt.toString().length < 2 ? `${imageInt}` : `${imageInt}`;
-         imageInt <= 788 ? setImageNumber(newImageString) : setImageNumber('0');
+       const newImageString = imageInt.toString().length < 2 ? `0${imageInt}` : `${imageInt}`;
+         imageInt <= 788 ? setImageNumber(newImageString) : setImageNumber('00');
      }
    }
-   handleSetImageNumber(imageInt);
+   handleSetImageNumber(+imageInt);
   }, [])
 
   const handleIncrementImageNumber = useCallback(() => {
     function handleIncrementImageNumberInner() {
       const imageInt = +imageNumber + 1;
-      handleSetImageNumber(imageInt)
+      handleSetImageNumber(imageInt);
     }
     handleIncrementImageNumberInner();
   }, [handleSetImageNumber, imageNumber]);
@@ -39,7 +43,7 @@ export default function Photos() {
 
     return () => {
       clearInterval(interval);
-    }
+    };
   }, [isPaused, intervalValue, handleIncrementImageNumber]);
 
   function handleTogglePause() {
@@ -57,21 +61,43 @@ export default function Photos() {
     if (!isPaused) {
       handleTogglePause();
     }
-    if (['A01', 'A02', 'A03', 'A04', 'A05', 'A06'].includes(e.target.value)) {
-      setImageFamily(e.target.value)
-    }
+    setImageFamily(e.target.value);
   }
 
   return (
     <>
       <div>
         <button onClick={handleTogglePause}>Toggle Pause</button>
-        <input onChange={handleSetIntervalValue}/>
-        <input onChange={handleSetImageFamily}/>
-        <input onChange={e => handleSetImageNumber(e.target.value)}/>
+        <label style={{ padding: "10px" }}>
+          Milliseconds per frame:
+          <input onChange={handleSetIntervalValue} value={intervalValue} />
+        </label>
+        <label style={{ padding: "10px" }}>
+          Image family:
+          <select onChange={handleSetImageFamily} value={imageFamily}>
+            {IMAGE_FAMILIES.map(imageFamily => {
+              return (
+                <option key={imageFamily} value={imageFamily}>
+                  {imageFamily}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+        <label style={{ padding: "10px" }}>
+          Image number:
+          <input
+            onChange={e => handleSetImageNumber(e.target.value)}
+            value={imageNumber}
+          />
+        </label>
       </div>
       <div>{`${imageNumber}_${imageFamily}_blended.png`}</div>
-      <img style={{maxWidth: '90vw', maxHeight: '90vh'}} src={imageSrc} alt="imageSrc"/>
+      <img
+        style={{ maxWidth: "90vw", maxHeight: "90vh" }}
+        src={imageSrc}
+        alt="imageSrc"
+      />
     </>
-  )
+  );
 }
